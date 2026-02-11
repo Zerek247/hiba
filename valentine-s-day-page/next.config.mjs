3bfadcf2fc4b4c4f1475fedb4d0bfb5f1926f2c9
Arturo <monster.304@outlook.es>
2026-02-11 13:42:28 -0600

    addd

--- valentine-s-day-page/next.config.mjs
+++ valentine-s-day-page/next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  basePath: "/hiba",
+  assetPrefix: "/hiba/",
+
+  images: {
+    unoptimized: true
+  },
+
   typescript: {
     ignoreBuildErrors: true,
   }
